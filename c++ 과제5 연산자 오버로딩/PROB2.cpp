794ebf1a64cbec1ef51e6bcc070c30f431647a33
hodu42 <knn330@naver.com>
2022-10-14 18:13:00 +0900

    3번 과제까지 완료

--- c++ 과제5 연산자 오버로딩/PROB2.cpp
+++ c++ 과제5 연산자 오버로딩/PROB2.cpp
@@ -9,12 +9,12 @@ int main()
 	const int MAX = 5;
 	int arr1[MAX] = { 1,2,3,4,5 }, arr2[MAX], * p;
 	
-	/*arr2 = arr1;*/		// arr1은 메모리에서의 arr1의 첫번째인덱스 주소를 반환한다 하지만 arr2는 배열로 선언했기 때문에 대입이 불가능하다.
+	/*arr2 = arr1;*/		// arr1은 메모리에서의 arr1의 첫번째인덱스 주소를 반환한다 그리고 arr2또한 메모리에서의 arr2의 주소를 가져오기 때문에 대입이 불가능하다.
 	for (int i = 0; i < MAX; i++) {
 		arr2[i] = arr1[i];
 	}
 
-	//p = arr1; p는 arr1의 복사본. // 포인터 p가 arr1의 주소를 가리키기 때문에 복사되지않는다.
+	//p = arr1; p는 arr1의 복사본. // 포인터 p가 arr1의 주소를 가리키기 때문에 예상했던대로 복사되지않는다.
 	p = new int[MAX];
 	for (int i = 0; i < MAX; i++) {
 		p[i] = arr1[i];
