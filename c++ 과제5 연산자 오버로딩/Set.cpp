ec8aa893e0de36a10179668667455905faabf6dc
hodu42 <knn330@naver.com>
2022-10-16 14:49:36 +0900

    set start

---
+++ c++ 과제5 연산자 오버로딩/Set.cpp
@@ -0,0 +1,26 @@
+#include "Set.h"
+
+const int INITIALSIZE = 10;
+
+Set::Set() { 
+	p = new int[INITIALSIZE];
+	crtSize = 0;
+	maxSize = INITIALSIZE;
+}
+void Set::insert(const int inputNum) {
+	if (inputNum > p[])
+}
+const Set Set::operator |(const Set) const {
+
+}
+const Set Set::operator &(const Set) const {
+
+}
+
+istream& operator >>(istream& inputStream, Set& set) {
+
+}
+
+ostream& operator <<(ostream& outputStream, const Set& set) {
+
+}
